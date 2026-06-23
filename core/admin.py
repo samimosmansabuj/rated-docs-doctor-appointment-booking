3332cc232c6bd9a8fd93fb7a98d76943fbbc780f
Samim Osman Sabuj <samim.o.sabuj01@gmail.com>
2026-06-23 20:08:09 +0600

    post log admin added!!!

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Procedure, LicenseRegistrationAuthority
+from .models import Procedure, LicenseRegistrationAuthority, PostMethodPayloadStore
 
 admin.site.register(Procedure)
 admin.site.register(LicenseRegistrationAuthority)
+admin.site.register(PostMethodPayloadStore)
